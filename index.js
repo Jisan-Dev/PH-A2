955d11ea6229698be1aa94b086c787144019fc86
Jisun696 <istiakkashem35@gmail.com>
2024-01-16 22:01:37 +0600

    added js file

---
+++ index.js
@@ -0,0 +1,8 @@
+function showsidebar() {
+  const sidebar = document.querySelector('.header__mobileMenu');
+  sidebar.style.display = 'flex';
+}
+function hidesidebar() {
+  const sidebar = document.querySelector('.header__mobileMenu');
+  sidebar.style.display = 'none';
+}
